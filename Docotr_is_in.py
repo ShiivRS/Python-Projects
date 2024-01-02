99c42c90d9ca372e5fb4332586e9eaf76b9fc588
Shiiv R S <86281810+ShiivRS@users.noreply.github.com>
2024-01-02 14:51:01 +0530

    Create Docotr_is_in.py

---
+++ Docotr_is_in.py
@@ -0,0 +1,9 @@
+print("The doctor is in.")
+p=0
+n=1
+while(n==1):
+  p+=1
+  print("Patient number ",p,"Come in")
+  n=int(input("Continue to next patient ?(0/1): "))
+
+print("The doctor is out.")
